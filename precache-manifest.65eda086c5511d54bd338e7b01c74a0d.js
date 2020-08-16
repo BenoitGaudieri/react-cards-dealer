3f0054db232187681e73e30c8973adb59a831230
BenoitGaudieri <benalek@gmail.com>
2020-08-16 18:55:11 +0200

    Updates

---
+++ precache-manifest.65eda086c5511d54bd338e7b01c74a0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e138f0c26b1afffed6775b52241698a2",
+    "url": "/react-cards-dealer/index.html"
+  },
+  {
+    "revision": "9d24105b86c49c24f3c6",
+    "url": "/react-cards-dealer/static/css/main.6d3a409a.chunk.css"
+  },
+  {
+    "revision": "2a1fc137f06153d145b6",
+    "url": "/react-cards-dealer/static/js/2.961944a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cards-dealer/static/js/2.961944a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d24105b86c49c24f3c6",
+    "url": "/react-cards-dealer/static/js/main.e37b1cb7.chunk.js"
+  },
+  {
+    "revision": "65677ad50b33d42ea202",
+    "url": "/react-cards-dealer/static/js/runtime-main.e02bc865.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cards-dealer/static/media/logo.5d5d9eef.svg"
+  }
+]);
